88c07529cedb9f1922801a763104cbcaa715c7b5
Riteshkumar12345 <122080902+Riteshkumar12345@users.noreply.github.com>
2025-03-31 11:16:32 +0000

    variable

---
+++ javascript/variable.js
@@ -0,0 +1,7 @@
+const accountId = "12233"
+let accountEmail = "ritesh@gmail.com"
+var  accountPassword = "111"
+accountCity = "UttarPredesh"
+
+
+console.table([accountId , accountEmail,accountPassword,accountCity]);
