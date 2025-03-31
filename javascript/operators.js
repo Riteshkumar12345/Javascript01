e0c87d4856673994ccc6def8fad3a4b809c8a30b
Riteshkumar12345 <122080902+Riteshkumar12345@users.noreply.github.com>
2025-03-31 12:39:35 +0000

    operators done

---
+++ javascript/operators.js
@@ -0,0 +1,10 @@
+//console.log("2" > 1);
+//console.log(null > 0);
+ //console.log(null == undefined); // == this opertor was loose equilty cheack thus it could not convert null to zero
+// "=="  this operator was convert null to undefind 
+//console.log(null >= 0); //  this operator was convert to null to 0 
+// undefined was give false in all this case
+console.log("2" === 2);
+// == this opertor was loose equilty 
+// === this operator waas strict equilty it was check both  value and type of datatype
+
