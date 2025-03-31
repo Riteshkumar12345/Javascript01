1f94c48f0b4b3d4f9f819ce05dd884401c1cae1a
Riteshkumar12345 <122080902+Riteshkumar12345@users.noreply.github.com>
2025-03-31 11:41:56 +0000

    conversion

---
+++ javascript/conversion.js
@@ -0,0 +1,14 @@
+let score =null
+console.log(typeof score);
+
+
+console.log(typeof score);
+
+let numberInInteger = Number(score);
+console.log(typeof numberInInteger);
+console.log(numberInInteger);
+
+// if declare underfined then output was underinfined
+// null => then output NaN
+// every string NaN
+// null ka type object
