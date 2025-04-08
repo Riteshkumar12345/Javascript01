edddd4345734d11931f18cce928f389c6eae3498
Riteshkumar12345 <122080902+Riteshkumar12345@users.noreply.github.com>
2025-04-08 18:16:20 +0000

    memory done

--- javascript/memory.js
+++ javascript/memory.js
@@ -1,5 +1,6 @@
 //stack (primtive) , heap(non-primtive)
-
+//primitive vo hmesaha copy create karta h aur heap vo hmesha refernce deta h aur sume cange karne se original valur me change hota h lekin primtive me aisa nhi hota h
+//primtive copy banta h jisse uske original value me change nhi hota h 
 let userOne = {
     email : "ritesh@gmial.com",
     upi :"ritesh@ybl"
