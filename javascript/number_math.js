98ef748aa734dff4d5f9a1715d3a4b91d11d8b96
Riteshkumar12345 <122080902+Riteshkumar12345@users.noreply.github.com>
2025-04-09 10:49:11 +0000

    all number and math are done

--- javascript/number_math.js
+++ javascript/number_math.js
@@ -1,18 +1,27 @@
 const score = 400
 console.log(score);
 // nunber ko hmlog do tarh se declare karte h 
-const balance = new Number(100);
-console.log(balance);
+// const balance = new Number(100);
+// console.log(balance);
 
 // lekin ye wala method thoda shi isse hmlog adik property ka use krte h 
 //console.log(balance.toFixed(2)); // 100.00
 //tofixed se hmlog dcimal ke baad ka kitni value dikani h vo batate h 
 
-const otherName = 123.833
-console.log(otherName.toPrecision(3));//124
-
-const hundread = 100000
-console.log(hundread.toLocaleString('en-IN'));
+// const otherName = 123.833
+// console.log(otherName.toPrecision(3));//124
 
 
 
+// Math
+console.log(Math.floor((Math.random() * 10) + 1));
+
+// function newFunction() {
+//     const hundread = 100000;
+//     console.log(hundread.toLocaleString('en-IN'));
+// }
+
+//if max and min are decalre 
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() *(max-min + 1)) + min);
