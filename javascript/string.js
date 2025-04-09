711d0a5493d5f30f6e1e8203c7aa937e20312f27
Riteshkumar12345 <122080902+Riteshkumar12345@users.noreply.github.com>
2025-04-09 09:53:54 +0000

    string

---
+++ javascript/string.js
@@ -0,0 +1,9 @@
+const userName = "RiteshKumar"
+const address = "Renukoot"
+console.log(`hi my name is ${userName} i am belong to ${address}`);//string ko is tarah declare karte h 
+//space ko hatne ke liye trim
+const newString = "   ritesh  "
+//console.log(newString.trim());
+console.log(userName.__proto__);
+
+//console.log(userName.slice(-9,4));
