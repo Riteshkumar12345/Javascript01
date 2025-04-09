bdaa65ac74dcc60a26c314df9e2180fc7c3f5a02
Riteshkumar12345 <122080902+Riteshkumar12345@users.noreply.github.com>
2025-04-09 10:39:53 +0000

    number

---
+++ javascript/number_math.js
@@ -0,0 +1,18 @@
+const score = 400
+console.log(score);
+// nunber ko hmlog do tarh se declare karte h 
+const balance = new Number(100);
+console.log(balance);
+
+// lekin ye wala method thoda shi isse hmlog adik property ka use krte h 
+//console.log(balance.toFixed(2)); // 100.00
+//tofixed se hmlog dcimal ke baad ka kitni value dikani h vo batate h 
+
+const otherName = 123.833
+console.log(otherName.toPrecision(3));//124
+
+const hundread = 100000
+console.log(hundread.toLocaleString('en-IN'));
+
+
+
