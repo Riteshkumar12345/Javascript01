8bbd4673429585ffbc00f9ce36c864f03451553f
Riteshkumar12345 <122080902+Riteshkumar12345@users.noreply.github.com>
2025-04-12 05:43:50 +0000

    object done

---
+++ javascript/object.js
@@ -0,0 +1,39 @@
+
+// singleton jb hm constructor se object bante h tb singleton object banta h 
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1", //symbol ko use [] ye lga ke karte h nhi to vo string ke type me badal jayega 
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym]) // symbol ko object me aisi hi call karte h 
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
