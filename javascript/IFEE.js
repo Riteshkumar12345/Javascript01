f8e02e0ddde80c8a325bf1e44fd6df4daf9e12d0
Riteshkumar12345 <122080902+Riteshkumar12345@users.noreply.github.com>
2025-04-13 10:27:18 +0530

    Add files via upload

---
+++ javascript/IFEE.js
@@ -0,0 +1,8 @@
+// (function one() { // isko named ifee kahte h 
+//     console.log("one")
+// }());
+// ifee immediately invoked function ko run karta h aur ye khud ko khud hi call karta h
+//global variable ke polution se bachne ke liye ifee use hota h
+// (() =>{
+//     console.log("two")
+// })()//isko call krte h ( ) se
